fb238c0b03beb46f19e19d34470788968399105c
RodrigoGomez14 <rodrigogomez548@gmail.com>
2018-10-09 00:27:45 -0300

    Actualizacion 3.2
    Contador de burako iniciado

--- app/src/main/java/com/example/rodrigo/anotadoruniversal/Jugador.java
+++ app/src/main/java/com/example/rodrigo/anotadoruniversal/Jugador.java
@@ -1,6 +1,9 @@
 package com.example.rodrigo.anotadoruniversal;
 
+import android.text.Editable;
+
 public class Jugador {
+    private String nombre;
     private int puntos=0;
 
     public void sumarPuntos(int puntos){
@@ -16,6 +19,14 @@ public class Jugador {
     }
     public Jugador(){
 
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public int getPuntos() {
